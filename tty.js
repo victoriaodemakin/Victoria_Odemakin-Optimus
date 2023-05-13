bba4397e6b5e008f3d5251b7a928d571502e3242
Odemakin Victoria <77022457+victoriaodemakin@users.noreply.github.com>
2023-05-13 14:28:37 -0700

    delete buffer

--- tty.js
+++
@@ -1,5 +0,0 @@
-/**Name : tty Module
- * Description:Provides classes used by a text terminal.The tty command of terminal basically prints the file name of the terminal connected to standard input. 
- * Example: 
-
-  */
