cb2e7bfa344461707c73a069c36e7a76af7c96bc
Odemakin Victoria <77022457+victoriaodemakin@users.noreply.github.com>
2023-05-13 14:25:04 -0700

    delete buffer

--- net.js
+++
@@ -1,9 +0,0 @@
-/**Name : Net Module
- * Description: When working with the HTTPS module in Node.js, it is possible to encounter an error when the internet connection is lost or disrupted while the HTTPS request is being made. 
- * Example: const https = require('https');
-https.get('https://www.example.com', (res) => {
-    console.log(res.statusCode);
-}).on('error', (err) => {
-    console.error(err);
-})
-  */
