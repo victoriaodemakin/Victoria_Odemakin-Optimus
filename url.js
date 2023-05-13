1bd99a061761a07388354267001154ecc349a8b9
Odemakin Victoria <77022457+victoriaodemakin@users.noreply.github.com>
2023-05-13 14:27:52 -0700

    delete buffer

--- url.js
+++
@@ -1,8 +0,0 @@
-/**Name : URL Module
- * Description: The ‘url’ module provides utilities for URL resolution and parsing. The getters and setters implement the properties of URL objects on the class prototype, and the URL class is available on the global object
- * Example:const searchpar = new URLSearchParams("keya=vala&keyb=valb");
- 
-for (let pair of searchpar.entries()) {
-    console.log(pair[0] + ', ' + pair[1]);
-}
-  */
