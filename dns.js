faf613ddfe4b716efb147ee94ef9b0b2d741b64f
Odemakin Victoria <77022457+victoriaodemakin@users.noreply.github.com>
2023-05-13 14:23:07 -0700

    delete buffer

--- dns.js
+++
@@ -1,8 +0,0 @@
-/**Name : dns Module
- * Description: is a node module used to do name resolution facility which is provided by the operating system as well as used to do an actual DNS lookup.
- * Example: 
- * const dns = require('dns');
-    const rrtype="A";
-        dns.resolve('geeksforgeeks.org', rrtype, (err, records)
-            => console.log('records: %j', records));
-  */
