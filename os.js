9224e6f23a9cdff6ad7fbec36635dbb0f59fc647
Odemakin Victoria <77022457+victoriaodemakin@users.noreply.github.com>
2023-05-13 14:25:20 -0700

    delete buffer

--- os.js
+++
@@ -1,7 +0,0 @@
-/**Name : Os Module
- * Description: Node.js OS module provides information about the computer operating system
- * Example: 
-const os = require("os");
-let osVersion = os.version();
-console.log("OS Version:", osVersion);
-  */
