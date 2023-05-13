d244d147acfd5c33688e4ee825ee05f1f9851f50
Odemakin Victoria <77022457+victoriaodemakin@users.noreply.github.com>
2023-05-13 14:22:53 -0700

    delete buffer

--- crypto.js
+++
@@ -1,8 +0,0 @@
-/**Name : Crypto Module
- * Description:  Crypto module for Node JS helps developers to hash user passwords.
- * Example: Original Password : portalforgeeks
-Hashed Password : bbf13ae4db87d475ca0ee5f97e397248a23509fc10c82f
-1e3cf110b352c3ca6cc057955ace9d541573929cd7a74a
-280a02e8cb549136b43df7704caaa555b38a
-
-  */
